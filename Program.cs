1b70d851a8e6cf3ad123766a77153bccb41c6b03
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-18 17:01:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignments_3._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year to check if it's a leap year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            bool isLeap = IfYearIsLeap(year);
+            Console.WriteLine($"Is {year} a leap year? {isLeap}");
+        }
+
+        static bool IfYearIsLeap(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
